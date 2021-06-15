f93ab9ee67329a497c7b464143d51e38175749b3
Ewan Anderson <conspiiracyy@gmail.com>
2021-06-15 15:07:00 +1000

    Main Project

---
+++ Programing Project/Assets/Scripts/Crosshair Highlight.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrosshairHighlight : MonoBehaviour
+{
+
+    private Ray ray = new Ray();
+    public RaycastHit hitInteractable;
+    public LayerMask layerToHit;
+    public GraphicRaycaster raycaster;
+
+    public float rayLength = 10f;
+    public KeyCode boundKey; 
+
+    public int InteractableObject; 
+
+}
+
+void Start()
+{
+
+
+
+
+}
+
+private void Update()
+{
+    GameObject.Find("Crosshair").GetComponent<Image>();
+
+    ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    if (hitInteractable)
+    {
+        Crosshair.color = Color.green;
+    }
+
+    
+
+
+}
+
---
+++ Programing Project/Assets/Scripts/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public Slider timerSlider;
+       
+    public void MeterMode(float value)
+    {
+        timerSlider.value = value;
+    }
+
+
+    public int gameover
+    {
+        
+
+
+    }
+   
+    void Update()
+    {
+        if (timerSlider > 0)
+        {
+            
+        }
+        else if (value <= 0)
+        {
+            gameover = true;
+        }
+
+
+
+
+    }
+}
